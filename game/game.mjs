bcb14d50bc1cc15628aba741e6a5fa0b93d33fb8
Vladislav_747 <zippo585@gmail.com>
2020-09-25 04:00:05 +0500

    52 passed 20 left

--- game/game.mjs
+++ game/game.mjs
@@ -1,11 +1,12 @@
-import { commands } from './coreMechanic.mjs';
+import { commands } from './coreMechanic.mjs'
 
-var raznica = 0;
+var raznica = 0
+
+var fieldSizeBoundaries = []
 
 export function startGame(snake, meals, fieldSize, maxTicks) {
-    console.log({snake, meals, fieldSize, maxTicks}, "startGame");
     //Чтобы не врезаться в стену
-
+    fieldSizeBoundaries = fieldSize
 }
 
 //Движение объекта
@@ -17,73 +18,97 @@ const directionsShift = {
 }
 
 export function getNextCommand(snake, meal) {
-   // console.log({snake, meal}, "getNextCommand");
+    // console.log({snake, meal}, "getNextCommand");
     //Куда будем двигаться?
-    if(meal){
-        
+    console.log(fieldSizeBoundaries, 'fieldSizeBoundaries')
+    if (meal) {
         //Первый элмент массива это x а второй y
-        const [ mealX, mealY ] = meal.split(';');
+        const [mealX, mealY] = meal.split(';')
 
         //Голова змеи - split - Метод split возвращает новый массив. из 4;0 идет массив [4,0]
-        const [ headX, headY ] = snake[0].split(';');
+        const [headX, headY] = snake[0].split(';')
         //Шея змеи - split
-        const [ neckX, neckY ] = snake[1].split(';');
+        const [neckX, neckY] = snake[1].split(';')
         //Сдвиг головы относительно шеи по Оси X
-        const xShift = headX - neckX;
+        const xShift = headX - neckX
         //Сдвиг головы относительно шеи по Оси Y
-        const yShift = headY - neckY;
+        const yShift = headY - neckY
 
-        if(raznica == 0){
-            if(mealY > headY && headX == mealX && xShift == -1 ) return commands.TURN_LEFT;
-            if(mealY > headY && headX == mealX && xShift == 1 ) return commands.TURN_RIGHT;
-            if(mealY == headY && headX < mealX && yShift == 1) return commands.TURN_LEFT;
-            if(mealY == headY && headX > mealX && yShift == 1) return commands.TURN_RIGHT;
-            if(mealY == headY && headX > mealX && xShift == - 1) {
-                raznica = headX - mealX - 1;
-                return commands.FORWARD;
-            }
-        
-            if(mealY > headY && headX == mealX && yShift == 1 ) {
-                raznica = mealY - headY - 1;
-                return commands.FORWARD;
-            }
-            if(mealY < headY && headX == mealX && yShift == -1 ) {
-                raznica = headY - mealY - 1;
-                return commands.FORWARD;
+        if (raznica == 0) {
+            //Еда находится параллельно
+            if (mealY > headY && headX == mealX && xShift == -1)
+                return commands.TURN_LEFT
+            if (mealY > headY && headX == mealX && xShift == 1)
+                return commands.TURN_RIGHT
+            if (mealY == headY && headX < mealX && yShift == 1)
+                return commands.TURN_LEFT
+            if (mealY == headY && headX > mealX && yShift == 1)
+                return commands.TURN_RIGHT
+
+            //Еда находится позади
+            if (mealY > headY && headX == mealX && yShift == -1)
+                return commands.TURN_RIGHT
+
+            if (mealY > headY && headX > mealX && yShift == -1)
+                return commands.TURN_LEFT
+
+            //Еда находится параллельно
+
+            if (mealY == headY && headX > mealX && xShift == -1) {
+                raznica = headX - mealX - 1
+                return commands.FORWARD
             }
 
-            if(mealY < headY && headX == mealX && xShift == 1 ) {
-                return commands.TURN_LEFT;
+            if (mealY > headY && headX == mealX && yShift == 1) {
+                raznica = mealY - headY - 1
+                return commands.FORWARD
             }
-            if(mealY < headY && headX == mealX && xShift == -1 ) {
-                return commands.TURN_RIGHT;
+            if (mealY < headY && headX == mealX && yShift == -1) {
+                raznica = headY - mealY - 1
+                return commands.FORWARD
             }
 
-            if(mealY == headY && headX > mealX && xShift == 1) {
-                raznica = headX - mealX - 1;
-                return commands.FORWARD;
+            if (mealY < headY && headX == mealX && xShift == 1) {
+                return commands.TURN_LEFT
             }
-            if(mealY == headY && headX < mealX && xShift == 1) {
-                raznica = mealX - headX - 1;
-                return commands.FORWARD;
+            if (mealY < headY && headX == mealX && xShift == -1) {
+                return commands.TURN_RIGHT
+            }
+
+            if (mealY == headY && headX > mealX && xShift == 1) {
+                raznica = headX - mealX - 1
+                return commands.FORWARD
+            }
+            if (mealY == headY && headX < mealX && xShift == 1) {
+                raznica = mealX - headX - 1
+                return commands.FORWARD
             }
 
             //Диагональные движения
-            if(mealY > headY && headX > mealX && xShift == 1) return commands.TURN_RIGHT;
-            if(mealY > headY && headX > mealX && yShift == 1) return commands.TURN_RIGHT;
-            if(mealY > headY && headX < mealX && yShift == 1 ) return commands.TURN_LEFT;
-            if(mealY > headY && headX < mealX && xShift == 1) return commands.TURN_RIGHT;
+            if (mealY > headY && headX > mealX && xShift == 1)
+                return commands.TURN_RIGHT
+            if (mealY > headY && headX > mealX && yShift == 1)
+                return commands.TURN_RIGHT
+            if (mealY > headY && headX < mealX && yShift == 1)
+                return commands.TURN_LEFT
+            if (mealY > headY && headX < mealX && xShift == 1)
+                return commands.TURN_RIGHT
 
             //Диагональные движения
-            if(mealY < headY && headX > mealX && xShift == -1) return commands.TURN_RIGHT;
-            if(mealY > headY && headX > mealX && xShift == -1) return commands.TURN_LEFT;
+            if (mealY < headY && headX > mealX && xShift == -1)
+                return commands.TURN_RIGHT
+            if (mealY > headY && headX > mealX && xShift == -1)
+                return commands.TURN_LEFT
 
-            if(mealY < headY && headX > mealX && yShift == -1) return commands.TURN_LEFT;
-    
-        }else{
-            raznica--;
-            return commands.FORWARD;
+            if (mealY < headY && headX > mealX && yShift == -1)
+                return commands.TURN_LEFT
+            if (mealY < headY && headX < mealX && yShift == -1)
+                return commands.TURN_RIGHT
+            if (mealY < headY && headX < mealX && xShift == 1)
+                return commands.TURN_LEFT
+        } else {
+            raznica--
+            return commands.FORWARD
         }
     }
-    
 }
--- game/index.mjs
+++ game/index.mjs
@@ -1,39 +1,47 @@
-import { runGame } from './coreMechanic.mjs';
-import { startGame, getNextCommand } from './game.mjs';
-import { renderField } from './visualizer.mjs';
+import { runGame } from './coreMechanic.mjs'
+import { startGame, getNextCommand } from './game.mjs'
+import { renderField } from './visualizer.mjs'
 
 //Отсчет ведем с нуля
 // const snake = [ '4;0', '3;0', '2;0', '1;0', '0;0' ];
 // const meals = [ '3;5', '8;6'];
 // const fieldSize = 10;
 // const maxTicks = 100;
-const snake = ["5;9", "5;8", "5;7", "5;6"];
-const meals = ["3;9", "2;4"];
-const fieldSize = 10;
-const maxTicks = 30;
+const snake = ['5;4', '5;5', '5;6', '5;7']
+const meals = ['2;6']
+const fieldSize = 10
+const maxTicks = 10
 
 async function run() {
     /*Объект игры
         сама змея - snake - параметры ее расположения
 
     */
-   //runGame - функция генератор
-    const iterGame = runGame(snake, meals, fieldSize, maxTicks);
+    //runGame - функция генератор
+    console.log('Проходим тест Еда сзади и сбоку от змеи:')
+    const iterGame = runGame(snake, meals, fieldSize, maxTicks)
     //runGame - функция генератор позволяет с помощью функцию next каждое значение при изменении
-    let gameState = iterGame.next().value;
-    console.log(gameState, "gameState");
+    let gameState = iterGame.next().value
+
     //Передаем изначальные данные (положение семьи, положение еды, размер поля, количество движений змеи)
     //наша функция
-    startGame([...snake], [...meals], fieldSize, maxTicks);
+    startGame([...snake], [...meals], fieldSize, maxTicks)
 
     do {
-        await renderField([...gameState.snake], gameState.currentMeal, fieldSize);
-        const currentCommand = getNextCommand([...gameState.snake], gameState.currentMeal);
-        console.log(currentCommand, "comand");
-        gameState = iterGame.next(currentCommand).value;
-    } while (!gameState.gameOver);   
+        await renderField(
+            [...gameState.snake],
+            gameState.currentMeal,
+            fieldSize
+        )
+        const currentCommand = getNextCommand(
+            [...gameState.snake],
+            gameState.currentMeal
+        )
+        console.log(currentCommand, 'comand')
+        gameState = iterGame.next(currentCommand).value
+    } while (!gameState.gameOver)
 
-    console.log(`Your Score is ${gameState.maxScore}`);
+    console.log(`Your Score is ${gameState.maxScore}`)
 }
 
-run();
+run()
